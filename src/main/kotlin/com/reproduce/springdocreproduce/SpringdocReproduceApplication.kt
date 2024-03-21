a4332f74b13eb82f545ca8b74d070671119c11ba
Stian Steinbakken <stian.steinbakken@posten.no>
2024-03-21 09:58:59 +0100

    chore: spring initializer

---
+++ src/main/kotlin/com/reproduce/springdocreproduce/SpringdocReproduceApplication.kt
@@ -0,0 +1,10 @@
+package com.reproduce.springdocreproduce
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication class SpringdocReproduceApplication
+
+fun main(args: Array<String>) {
+  runApplication<SpringdocReproduceApplication>(*args)
+}
---
+++ src/test/kotlin/com/reproduce/springdocreproduce/SpringdocReproduceApplicationTests.kt
@@ -0,0 +1,10 @@
+package com.reproduce.springdocreproduce
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringdocReproduceApplicationTests {
+
+  @Test fun contextLoads() {}
+}
